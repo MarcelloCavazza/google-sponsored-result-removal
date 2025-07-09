cef613f407820dd3bf98f2ac9df4e1b5c666e48e
Marcello Cavazza <marcellocavazzaoliveira.dev@gmail.com>
2025-07-09 17:37:36 -0300

    fix: description text

--- google_sponsored_result_removal.js
+++ google_sponsored_result_removal.js
@@ -2,7 +2,7 @@
 // @name         Google Sponsored result removal
 // @namespace    http://tampermonkey.net/
 // @version      1.0.0
-// @description  Google Sponsored result removal
+// @description  Google Sponsored result removal from search
 // @author       Marcello Cavazza
 // @match        https://www.google.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
